45cd2751ae1a6ebaf1598c3bfe0a625c0237891f
alexlbr <alex.lobera@gmail.com>
2018-12-02 11:46:02 +0000

    adds codeSplitting example

--- examples/codeSplitting/src/App.js
+++ examples/codeSplitting/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Suspense } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 const LazyComponent = React.lazy(() => import("./LazyComponent"));
@@ -7,41 +7,33 @@ const LazyComponent = React.lazy(() => import("./LazyComponent"));
 class App extends Component {
   render() {
     return (
-      <Router>
-        <>
-          <Route
-            path="/"
-            exact
-            render={() => (
-              <div className="App">
-                <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  <Link to="/lazy">Get lazy></Link>
-                  <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Learn React1
-                  </a>
-                </header>
-              </div>
-            )}
-          />
-          <Route
-            path="/lazy"
-            render={() => (
-              <Suspense fallback={<i>Loading...</i>}>
-                <LazyComponent />
-              </Suspense>
-            )}
-          />
-        </>
-      </Router>
+      <>
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Link className="App-link" to="/lazy">
+                  Get lazy>
+                </Link>
+              </header>
+            </div>
+          )}
+        />
+        <Route
+          path="/lazy"
+          render={() => (
+            <Suspense fallback={<i>Loading...</i>}>
+              <LazyComponent />
+            </Suspense>
+          )}
+        />
+      </>
     );
   }
 }
--- examples/codeSplitting/src/server.js
+++ examples/codeSplitting/src/server.js
@@ -1,5 +1,6 @@
+import React from "react";
+import { StaticRouter as Router } from "react-router-dom";
 import App from "./App";
-const React = require("react");
 const express = require("express");
 const { createSSRMiddleware } = require("react-scripts-ssr");
 const { renderToString } = require("react-dom/server");
@@ -8,7 +9,13 @@ const server = express();
 
 server.use(
   createSSRMiddleware((req, res, next) => {
-    const body = renderToString(<App />);
+    const body = renderToString(
+      <Router location={req.url}>
+        <App />
+      </Router>
+    );
+    console.log("body", body);
+
     next({ body }, req, res);
   })
 );
